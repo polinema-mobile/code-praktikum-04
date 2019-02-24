bfd379d4e02e55fa75e978db3bd81fd533c40511
Putra Prima Arhandi <putraprima@gmail.com>
2019-02-24 14:04:41 +0700

    intent dan luas

--- app/src/main/java/com/recyclerview/putraprima/myapplication/LuasActivity.java
+++ app/src/main/java/com/recyclerview/putraprima/myapplication/LuasActivity.java
@@ -1,13 +1,38 @@
 package com.recyclerview.putraprima.myapplication;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LuasActivity extends AppCompatActivity {
+    private EditText edtPanjang;
+    private EditText edtLebar;
+    private Button btnLuas;
+    private TextView txtLuas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luas);
+
+        edtLebar = findViewById(R.id.edtLebar);
+        edtPanjang = findViewById(R.id.edtPanjang);
+        btnLuas = findViewById(R.id.btnLuas);
+        txtLuas = findViewById(R.id.txtLuas);
+
+        btnLuas.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                Float Luas = (float) (Integer.parseInt(edtLebar.getText().toString()) * Integer.parseInt(edtPanjang.getText().toString()));
+                txtLuas.setText(Luas.toString());
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/recyclerview/putraprima/myapplication/MainActivity.java
+++ app/src/main/java/com/recyclerview/putraprima/myapplication/MainActivity.java
@@ -1,13 +1,31 @@
 package com.recyclerview.putraprima.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLuas,btnCalculator,btnRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLuas = findViewById(R.id.btnLuas);
+        btnCalculator = findViewById(R.id.btnCalculator);
+        btnRecycler = findViewById(R.id.btnRecycler);
+
+        btnLuas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),LuasActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
